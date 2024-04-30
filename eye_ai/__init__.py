5f0d781143bf99301975150256d1caabd746a15d
Carl Kesselman <carl@isi.edu>
2024-04-29 21:31:21 -0700

    Modified init so it will work without package.

--- eye_ai/__init__.py
+++ eye_ai/__init__.py
@@ -1,7 +1,17 @@
 from importlib.metadata import version, PackageNotFoundError
+from setuptools_git_versioning import version_from_git
+import subprocess
+
+import os
+from pathlib import Path
+
+in_repo = (Path(__file__).parents[1] / Path(".git")).is_dir()
 
 try:
-    __version__ = version("eye-ai")
+    if  in_repo:
+        __version__ = subprocess.check_output(["setuptools-git-versioning"], text=True)[:-1] 
+    else: 
+        __version__ = version("deriva_ml")
 except PackageNotFoundError:
     # package is not installed
     pass
